7d607178b1900a1d30563096658abe5284c5b44e
Rasel <56032652+rasel-bhuiyan@users.noreply.github.com>
2020-04-05 16:32:21 +0600

    Add files via upload

---
+++ Employee.sql
@@ -0,0 +1,24 @@
+
+
+create table Employee(
+E_ID number(5);
+F_Name varchar(10);
+M_Name varchar(10);
+L_Name varchar(8);
+Gender varchar(6);
+DOB date;
+
+);
+
+
+create table  Customer(
+C_ID number(5);
+F_Name varchar(10);
+M_Name varchar(10);
+L_Name varchar(8);
+Gender varchar(6);
+DOB date;
+);
+
+
+
